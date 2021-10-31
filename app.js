3322155db24f389a02d2f533e8c597cd67e80f6c
ConroyCampbell <campbelljnrconroy@gmail.com>
2021-10-31 12:47:09 -0500

    Added js file

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 onload= function(){
     var btn=document.getElementById('btn');
     var httpRequest=new XMLHttpRequest();
-    var result=document.getElementById('result');
-    var result1=document.getElementById('result-tag');
-    var character=document.getElementById('textbox');
+    var character="";
     btn.addEventListener('click',function(event){
         event.preventDefault();
         var url="superheroes.php?charac_data="+character.value;
@@ -17,13 +15,9 @@ onload= function(){
         if(httpRequest.readyState===XMLHttpRequest.DONE){
             if (httpRequest.status===200){
                 var response=httpRequest.responseText;
-                result1.innerHTML="RESULT"
-                result.innerHTML=response;
-                //alert(response);
+                alert(response);
             }else{
-                result1.innerHTML="RESULT"
-                result.innerHTML="There was a problem with the request";
-                //alert('There was a problem with the request')
+                alert('There was a problem with the request')
             }
         }
     }
